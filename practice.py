ad9f8230cf93e3a01d7504e8e247615a8bf5ca9f
harsh <jangraharsh@gmail.com>
2026-05-07 16:13:44 +0530

    made a function which takes a list and gives - sum,highest,lowest,total

--- practice.py
+++ practice.py
@@ -1,9 +1,34 @@
-nums = [2, 4, 7, 9]
-
-def sum_list(nums):
+def analyze_list(numbers):
+    
     total = 0
-    for num in nums:
-        total += num
-    return total
+    highest = numbers[0]
+    lowest = numbers[0]
 
-print(sum_list(nums))
+
+    for num in numbers:
+        total += num
+    
+    for num in numbers:
+        if num > highest:
+            highest = num
+
+    for num in numbers:
+        if num < lowest:
+            lowest = num
+    
+    length = len(numbers)
+
+    return total , highest , lowest , length
+
+numbers = [5,2,8,1,9,3]
+
+total , hightest , lowest , length = analyze_list(numbers)
+
+
+
+
+
+print(f"sum : {total}")
+print(f"length : {length}")
+print(f"lowest : {lowest}")
+print(f"hightest : {hightest}")
