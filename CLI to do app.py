0c03abdc3569af558fc90dd62a2b9cb9d499e5cd
harsh <jangraharsh@gmail.com>
2026-05-14 17:45:15 +0530

    added functions

--- CLI to do app.py
+++ CLI to do app.py
@@ -1,5 +1,25 @@
 tasks = []
 
+def add_task():
+    task_1 = input("Enter task = ")
+    tasks.append(task_1)
+
+def view_task():
+    if len(tasks) == 0:
+        print("no tasks yet")
+    else:
+        for index ,task in enumerate(tasks,
+        start=1):
+            print(f"{index}. {task}")
+
+def delete_task():
+    deleting_task = input("Enter task to delete = ")
+    try:
+        tasks.remove(deleting_task)
+    except ValueError:
+        print("task not found")
+
+
 
 while True:
         user_input = input(f"""1. Add task
@@ -7,28 +27,20 @@ while True:
 3. Delete task
 4. Exit
 => """)
-        
+
+
+
         if user_input == "1":
-            task_1 = input("Enter task = ")
-            tasks.append(task_1)
+            add_task()
         
         elif user_input == "2":
-            if len(tasks) == 0:
-                print("no tasks yet")
-            else:
-                for index ,task in enumerate(tasks,
-                start=1):
-                    print(f"{index}. {task}")
+            view_task()
 
-
-    
         elif user_input == "3":
-            deleting_task = input("Enter task to delete = ")
-            try:
-                tasks.remove(deleting_task)
-            except ValueError:
-                print("task not found")
+            delete_task()
+
         elif  user_input == "4":
             break
+
         else:
             print("Invalid option") 
