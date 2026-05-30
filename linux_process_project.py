7c439fb079c66e55253390d63c449958281eb0f7
harsh <jangraharsh@gmail.com>
2026-05-30 13:08:12 +0530

    completed working linux process project

--- linux_process_project.py
+++ linux_process_project.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 class Process:
     def __init__(self, pid, name, state ):
         self.pid = pid
@@ -8,9 +10,9 @@ class Process:
         return f"{self.pid} | {self.name} | {self.state}"
 
     def start(self):
-        if self.state == "running":
-            print(f"{self.pid} | {self.name} => is running")
-
+        self.state = "running"
+        sleep(3)
+        print(f"{self.pid} | {self.name} => is running")
 
 class Systemd:
     def __init__(self):
@@ -20,12 +22,13 @@ class Systemd:
         self.next_pid = 1
 
     def create(self):
+
         name = input("Enter process to create ❯ ")
 
         process = Process(
-            pid= self.next_pid,
-            name= name,
-            state= "stopped"
+        pid= self.next_pid,
+        name= name,
+        state= "stopped"
             )
 
         self.processes[self.next_pid] = process
@@ -37,15 +40,40 @@ class Systemd:
             print(f"{x.pid} | {x.name} | {x.state}")
 
     def start_process(self):
+        try:
+            pid = int(input("Enter pid to start the process > "))
+        except ValueError:
+            print("Please enter a valid number.")
+            return
 
-        user_input = int(input(f"Enter pid to start the process ❯ "))
-        process = self.processes[user_input]
-        self.processes[user_input] = process
+        process = self.processes.get(pid)
 
-        print(f"{user_input} | {process.name} => has started")
+        if process is None:
+            print("PID not found.")
+            return
 
+        if process.state == "running":
+            print(f"{process.pid} | {process.name} => is already running")
+        else:
+            process.start()
 
-sys = Systemd()
+    def kill_process(self):
+        try:
+            kill_input = int(input("Enter pid to kill a process ❯ "))
+        except ValueError:
+            print("Please enter a valid number.")
+            return
+
+        process = self.processes.get(kill_input)
+
+        if process is None:
+            print("PID not found.")
+            return
+        self.processes.pop(kill_input)
+
+        print(f"{process.pid} | {process.name} => is killed")
+
+systemd = Systemd()
 
 
 def main():
@@ -53,23 +81,28 @@ def main():
         choice = input("""Enter 1 to create a process
 Enter 2 to start a process
 Enter 3 to show process
-Enter 4 to Exit
+Enter 4 to kill a process
+Enter 5 to Exit
 => """)
 
         if choice == "1":
-            sys.create()
-            sys.show_process()
+            systemd.create()
+            systemd.show_process()
             input("press enter to continue...")
 
         elif choice == "2":
-            sys.start_process()
+            systemd.start_process()
             input("press enter to continue...")
 
         elif choice == "3":
-            sys.show_process()
+            systemd.show_process()
             input("press enter to continue...")
 
         elif choice == "4":
+            systemd.kill_process()
+            input("press enter to continue...")
+
+        elif choice == "5":
             break
         else:
             print("Invalid input")
